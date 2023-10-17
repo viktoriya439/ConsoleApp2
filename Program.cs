4b71c77746f08b4ac0e132e6b4447c9a433c8296
viktoriya439 <lukyanenkoviktoriya439@gmail.com>
2023-10-18 00:20:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("What is your favorite day of the week?");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day of the week is {0}", day);
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is {0}", birthdate);
+            Console.ReadKey();
+
+        }
+
+        
+    }
+    }
+
